0c1aa055a0497d7199f61bbc21ed275a6c8483ab
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-03-14 00:04:25 +0600

    3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+const [values, setValues] = useState()
+
+useEffect(() => {
+  axios.get('http://localhost:5004/api/v1/cars')
+    .then(res => setValues(res))
+    .catch(err => console.log(err))
+},[])
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(values.data.data.cars)}
+      {values.data.data.cars.map((c) => (
+        <div key={c.id}>
+          <p>Mark: {c.mark}</p>
+          <p>Type: {c.car_type}</p>
+          <p>Model: {c.model}</p>
+        </div>
+      ))}
     </div>
   );
 }
